1cd7ccf4938898cdd09ce486679d101f0bc948c9
putc <putc@mail.ru>
2019-12-26 15:47:27 +0300

    my solution

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,20 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+from random import random
+
+LEFT = int(input("Минимальная граница: "))
+RIGHT = int(input("Максимальная граница: "))
+NUMB = int(random() * (RIGHT - LEFT + 1)) + LEFT
+print(NUMB)
+
+LEFT = float(input("Минимальная граница: "))
+RIGHT = float(input("Максимальная граница: "))
+NUMB = random() * (RIGHT - LEFT) + LEFT
+print(round(NUMB, 3))
+
+LEFT = ord(input("Минимальная граница: "))
+RIGHT = ord(input("Максимальная граница: "))
+LETTER = int(random() * (RIGHT - LEFT + 1)) + LEFT
+print(chr(LETTER))
--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,9 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+LETTER1 = ord(input("Введите первую букву"))
+LETTER2 = ord(input("Введите вторую букву"))
+LETTER1_ORDER=LETTER1-96
+LETTER2_ORDER=LETTER2-96
+LETTER_DIST=max(0,abs(LETTER1-LETTER2)-1)
+print(LETTER1_ORDER,LETTER2_ORDER,LETTER_DIST)
