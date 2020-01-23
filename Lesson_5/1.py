e9cf0aa71e20e96bef35b28d289383d8e6035008
putc <putc@mail.ru>
2020-01-23 14:20:52 +0300

    HW5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,47 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+
+import collections
+
+
+Enterprise = collections.namedtuple('Enterprise', ['q1', 'q2', 'q3', 'q4'])
+
+base_enterprise = {}
+
+n = int(input("Введите количество предприятий: "))
+
+for i in range(n):
+    name = input('Введите название предприятия: ')
+    q1_profit = int(input('Введите прибыль за 1-й квартал: '))
+    q2_profit = int(input('Введите прибыль за 2-й квартал: '))
+    q3_profit = int(input('Введите прибыль за 3-й квартал: '))
+    q4_profit = int(input('Введите прибыль за 4-й квартал: '))
+    base_enterprise[name] = Enterprise(
+        q1 = q1_profit,
+        q2 = q2_profit,
+        q3 = q3_profit,
+        q4 = q4_profit
+    )
+
+
+total_profit = ()
+
+for name, profit in base_enterprise.items():
+    print(f'Предприятие: {name} прибыль за год - {sum(profit)}')
+    total_profit += profit
+
+avg_profit_total = sum(total_profit) / len(base_enterprise)
+print(f'Средняя прибыль за год для всех предприятий {avg_profit_total}')
+
+print('Предприятия, у которых прибыль выше среднего:')
+
+for name, profit in base_enterprise.items():
+    if sum(profit) > avg_profit_total:
+        print(f'{name} - {sum(profit)}')
+
+print('Предприятия, у которых прибыль ниже среднего:')
+for name, profit in base_enterprise.items():
+    if sum(profit) < avg_profit_total:
+        print(f'{name} - {sum(profit)}')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,23 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+import collections
+
+
+def hex_val(num):
+    hv = 0
+    for i in range(len(num)):
+        hv += int(num[i], 16) * (16 ** (len(num) - i - 1))
+    return hv
+
+HEX_DICT = collections.defaultdict(list)
+A = input("Введите первое число: ")
+B = input("Введите второе число: ")
+HEX_DICT[A] = list(A)
+HEX_DICT[B] = list(B)
+
+HEX_SUM = str(hex(hex_val(HEX_DICT[A] ) + hex_val(HEX_DICT[B])))[2::]
+HEX_MULT = str(hex(hex_val(HEX_DICT[A] ) * hex_val(HEX_DICT[B] )))[2::]
+
+print(f"Сумма чисел {HEX_SUM}, Произведение {HEX_MULT}")
